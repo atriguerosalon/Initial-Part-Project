ef0fb5ebc886339d24fbf26d71be4e8f2c736bf6
A. Trigueros Alonso <131680968+atriguerosalon@users.noreply.github.com>
2024-03-14 10:52:12 +0100

    changed data prep

--- data_preparation.py
+++ data_preparation.py
@@ -87,8 +87,13 @@ def overlay_fields(phi, img_path, x, y, filename='overlay.pdf'):
 
 if __name__ == '__main__':
     # Usage of functions
+    exclude_boundaries = 5
     data_path_temp = 'nablatemp-slice-B1-0000080000.raw'
     data_path_reaction = 'wtemp-slice-B1-0000080000.raw'
     wcr_field_star, ct_field_star, phi = filename_to_field(data_path_temp, data_path_reaction, exclude_boundaries=0)
     # plot_fields(wcr_field_star, ct_field_star, phi)  # Uncomment to plot fields
+
+    # Plot phi field
+
+
     overlay_fields(phi, 'figs/figure7.png', x, y)  # Adjust 'figure7.png' to your image's path
