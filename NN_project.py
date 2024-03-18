b20f7b4615e9431b3725d7a0d86b8c3c3683be8d
ggelissen <g.m.j.gelissen@student.tudelft.nl>
2024-03-18 09:59:10 +0100

    ...

--- NN_project.py
+++ NN_project.py
@@ -30,13 +30,13 @@ sig = np.sqrt(fwidth_n[0] ** 2 / 12.0)
 
 # Data loading and processing placeholder - Replace with your actual data loading
 # Example for data_1
-data_1_path = "C:\\Users\\jdest\\Downloads\\mission1\\wtemp-slice-B1-0000080000.raw" # Update this path
+data_1_path = "wtemp-slice-B1-0000080000.raw" # Update this path
 data_1 = np.fromfile(data_1_path, count=-1, dtype=np.float64)
 data_1 = data_1.reshape(ny, nx)
 data_1_filtered = scipy.ndimage.gaussian_filter(data_1, sigma=sig)
 
 # Example for data_2
-data_2_path = "C:\\Users\\jdest\\Downloads\\mission1\\nablatemp-slice-B1-0000080000.raw"  # Update this path
+data_2_path = "nablatemp-slice-B1-0000080000.raw"  # Update this path
 data_2 = np.fromfile(data_2_path, count=-1, dtype=np.float64)
 data_2 = data_2.reshape(ny, nx)
 data_2_filtered = scipy.ndimage.gaussian_filter(data_2, sigma=sig)
@@ -53,7 +53,7 @@ data_3 = data_3 / (2.0 * DTH)
 X = np.stack((data_1_filtered.flatten(), data_2_filtered.flatten(), data_3.flatten()), axis=1)
 
 # Model load
-model = load_model('C:\\Users\\jdest\\Downloads\\mission2\\mission2\\phi_nn_epoch_00240.h5')
+model = load_model('phi_nn_epoch_00240.h5')
 
 # Prediction
 phi_nn = model.predict(X, batch_size=16, verbose=2)
