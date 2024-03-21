e6fbcd90d4322adafce075ab7bcc1a646310d020
ggelissen <g.m.j.gelissen@student.tudelft.nl>
2024-03-21 10:07:29 +0100

    ...

--- comparison.py
+++ comparison.py
@@ -98,8 +98,8 @@ using_datashader(ax, 1.0)
 plt.plot([0,1], [0,1], linestyle='--', marker='', c='black', lw=0.8)
 plt.xlim(0,1)
 plt.ylim(0,1)
-plt.ylabel("$\\bar{\\Phi}_{c,NN}$")
-plt.xlabel("$\\bar{\\Phi}_{c,res}$")
+plt.ylabel("$\\overline{\\Phi}_{NN}$")
+plt.xlabel("$\\overline{\\Phi}_{res}$")
 plt.show()
 
 #scatter_plot_run1(1.0)
@@ -107,8 +107,8 @@ white_jet = create_custom_cmap()
 def plot_comparison_graphs():
   for i in range(len(filter_sizes)):
     if i==0:
-      plt.subplot(2,len(filter_sizes),i+1).text(-171, 171, "$\\bar{\\Phi}_{c,res}$", fontsize=16, fontfamily='serif')
-      plt.subplot(2,len(filter_sizes),i+1+len(filter_sizes)).text(-171, 171, "$\\bar{\\Phi}_{c,NN}$", fontsize=16, fontfamily='serif')
+      plt.subplot(2,len(filter_sizes),i+1).text(-171, 171, "$\\overline{\\Phi}_{res}$", fontsize=16, fontfamily='serif')
+      plt.subplot(2,len(filter_sizes),i+1+len(filter_sizes)).text(-171, 171, "$\\overline{\\Phi}_{NN}$", fontsize=16, fontfamily='serif')
     plt.subplot(2,len(filter_sizes),i+1).axes.get_xaxis().set_visible(False)
     plt.subplot(2,len(filter_sizes),i+1).axes.get_yaxis().set_visible(False)
     plt.pcolor(phi_field_res(filter_sizes[i]), cmap='jet')
@@ -143,9 +143,18 @@ def comparison_plot(MSE_or_Pearson):
       y.append(calculate_MSE(i))
   else:
     print("comparison_plot only takes \'MSE\' or \'Pearson\'")
-  plt.plot(filter_sizes,y, 'b', marker='o')
+  plt.plot(filter_sizes,y, 'k', marker='o')
+  plt.vlines(filter_sizes[1:-1], 0, max(y), colors='gray', linestyles='dashed', alpha=0.3)
+  plt.xlim(0.22, 2.03)
+  plt.ylim(0.85*min(y), max(y))
+  plt.tick_params(axis='both', which='major', direction='in', top=True, right=True)
+  plt.xlabel("$\\Delta/\\delta_{th,norm}$")
+  if MSE_or_Pearson=="MSE":
+    plt.ylabel("$\\epsilon_{MSE}$")
+  else:
+    plt.ylabel("$r_{p}$")
   plt.show()
-comparison_plot('MSE')
+comparison_plot('Pearson')
 '''
 
 
