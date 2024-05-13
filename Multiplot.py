0ef9ad582341a17c61c43dc11216d58328bc645b
A. Trigueros Alonso <131680968+atriguerosalon@users.noreply.github.com>
2024-05-13 10:03:54 +0200

    updated figs

--- Multiplot.py
+++ Multiplot.py
@@ -12,9 +12,10 @@ plt.rcParams['font.family'] = 'STIXGeneral'
 fwidth_n = np.array([0, 25, 37, 49, 62, 74, 86, 99])
 filter_sizes = [0, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0]
 
-filter_size = filter_sizes[3]
+idx = 3
+filter_size = filter_sizes[idx]
 # std. deviation
-sigma_value = np.sqrt(fwidth_n[3] ** 2 / 12.0)
+sigma_value = np.sqrt(fwidth_n[idx] ** 2 / 12.0)
 print(f'sigma_value: {sigma_value}')
 #hi Job
 # Exclusion boundaries
@@ -42,6 +43,7 @@ wcr_field_filtered = gaussian_filter(wcr_field_star, sigma=sigma_value)
 ct_field_filtered = gaussian_filter(ct_field_star, sigma=sigma_value)
 phi_res_filtered = gaussian_filter(phi, sigma=sigma_value)
 
+'''
 #Print maximum wcr_field_filtered value
 print(f"Maximum value in wcr_field_filtered: {np.max(wcr_field_filtered)}")
 
@@ -50,7 +52,7 @@ print(f"Maximum value in ct_field_filtered: {np.max(ct_field_filtered)}")
 
 #Print maximum phi_res_filtered value
 print(f"Maximum value in phi_res_filtered: {np.max(phi_res_filtered)}")
-
+'''
 #Create custom color map
 white_jet = create_custom_cmap()
 
@@ -68,24 +70,31 @@ extent_mm = [new_horizontal_extent_start_mm, new_horizontal_extent_end_mm, 0, 10
 
 # Plotting the original fields
 axs[0, 0].imshow(wcr_field_star, cmap=white_jet, extent=extent_mm, vmin=0, vmax=1)
+# Maximum value in original wcr_field_star
+max_wcr = np.max(wcr_field_star)
 #axs[0, 0].set_title('(a) ωcT*')
 
 axs[0, 1].imshow(ct_field_star, cmap=white_jet, extent=extent_mm, vmin=0, vmax=1)
+# Maximum value in ct_field_star
+max_ct = np.max(ct_field_star)
 #axs[0, 1].set_title('(b) |∇cT|*')
 
 axs[0, 2].imshow(phi, cmap=white_jet, extent=extent_mm, vmin=0, vmax=1)
+# Maximum value in phi
+max_phi = np.max(phi)
 #axs[0, 2].set_title('(c) Φres')
 
 #maximum value in filtered fields
 max_filtered_value = max(np.max(wcr_field_filtered), np.max(ct_field_filtered), np.max(phi_res_filtered))
+print(f"Maximum value in filtered fields: {max_filtered_value}")
 # Plotting the filtered fields
-axs[1, 0].imshow(wcr_field_filtered, cmap=white_jet, extent=extent_mm, vmin=0, vmax=1)
+axs[1, 0].imshow(wcr_field_filtered, cmap=white_jet, extent=extent_mm, vmin=0, vmax=max_wcr)
 #axs[1, 0].set_title('(d) ωcT* filtered')
 
-axs[1, 1].imshow(ct_field_filtered, cmap=white_jet, extent=extent_mm, vmin=0, vmax=1) #ask alejandro
+axs[1, 1].imshow(ct_field_filtered, cmap=white_jet, extent=extent_mm, vmin=0, vmax=max_ct) #ask alejandro
 #axs[1, 1].set_title('(e) |∇cT|* filtered')
 
-axs[1, 2].imshow(phi_res_filtered, cmap=white_jet, extent=extent_mm, vmin=0, vmax=1)
+axs[1, 2].imshow(phi_res_filtered, cmap=white_jet, extent=extent_mm, vmin=0, vmax=max_phi)
 #axs[1, 2].set_title('(f) Φres filtered')
 
 # Set the labels and titles as per your requirement
